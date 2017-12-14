8f92339585dad01e32fe514dec1842e178f31d4e
Peter Juffernbruch <peterjuffernbruch@Peters-MacBook-Pro.local>
2017-12-14 07:52:06 -0600

    css basic, js improvements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,62 @@
 //assign variables for, hidden-random number, crystals, wins, losses, your total score,
 var randNumb;
-var emCrystal;
-var yellCrystal;
-var rubyCrystal;
-var blueCrystal;
+var green;
+var yellow;
+var red;
+var blue;
 var totalScore;
 var wins = 0;
 var losses = 0;
 
 
 //comp will randomly generate a number between 19-120
-function getRandomNumb() {
-    return Math.floor((Math.random() * 120) + 19);
-   console.log(getRandomNumb);
-}
+var randNumb = Math.floor((Math.random() * 120) + 19);
+   console.log(randNumb);
+
  
-$("#randNumb") === getRandomNumb;
+$("#randNumb").html(randNumb);
 
 //assign random value to each crystal that changes with each round
 
-var emCryNum = Math.floor(Math.random() * 12) + 1;
-var yellCryNum = Math.floor(Math.random() * 12) + 1;
-var rubCryNum = Math.floor(Math.random() * 12) + 1;
-var bluCryNum = Math.floor(Math.random() * 12) + 1;
+var green = Math.floor(Math.random() * 12) + 1;
+   newDiv = $("<div>"); 
+   newDiv.text(green);
+   $("#green").append(newDiv);
+
+    console.log(green);
+
+var yellow = Math.floor(Math.random() * 12) + 1;
+    $("#yellow").html(yellow);
+    $("#yellow").append(yellow);
+    $("#yellow").on("click", function(){
+        var yellowValue = ($(this).append("yellow"));
+})
+
+    console.log(yellow);
+
+var red = Math.floor(Math.random() * 12) + 1;
+    $("#red").html(red);
+    $("#red").append(red);
+    $("#red").on("click", function(){
+        var redValue = ($(this).append("red"));
+})
+
+    console.log(red);
+
+var blue = Math.floor(Math.random() * 12) + 1;
+    $("#blue").html(blue);
+    $("#blue").append(blue);
+    $("#blue").on("click", function(){
+        var blueValue = ($(this).append("blue"));
+})
+
+    console.log(blue);
 //if statements to keep track of score
+
+if (totalScore === randNumb) {
+    alert("You Win!!");
+}
+
+else if (totalScore === randNumb) {
+    alert("You Lose! Try Again!");
+}
